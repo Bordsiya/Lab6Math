541f4fd11bc4302f2eea2cf7fdb85791d3d002d1
Bordsiya <bordun.2002@mail.ru>
2022-05-22 22:09:42 +0300

    Update main.py

--- Lab6Math/main.py
+++ Lab6Math/main.py
@@ -1,9 +1,10 @@
-from Methods.MethodsUtils import do_method
+from Methods.MethodsUtils import do_method, calculate_final_table_with_accuracy
 from Utils.GraphMethods import draw_graph
 from Utils.IOMethods import get_data, get_method, print_table
 
-differential_equation = get_data()
+differential_equation_h = get_data()
 method_id = get_method()
-table, headers = do_method(method_id, differential_equation)
-print_table(table, headers)
-draw_graph(table, differential_equation)
+table_h, headers_h = do_method(method_id, differential_equation_h)
+table_h, headers_h = calculate_final_table_with_accuracy(method_id, differential_equation_h, table_h, headers_h)
+print_table(table_h, headers_h)
+draw_graph(table_h, differential_equation_h)
