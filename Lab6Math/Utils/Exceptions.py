ee5db4ad86174c4e3f2d0a66004ca8dc0188b076
Bordsiya <bordun.2002@mail.ru>
2022-05-23 17:52:07 +0300

    Delete Lab6Math directory

--- Lab6Math/Utils/Exceptions.py
+++
@@ -1,8 +0,0 @@
-
-exceptions_arr = {
-    'ValueError': 'Введены данные некорректного типа',
-    'WrongLimitsArgument': 'Введенные данные не соответсвуют ни одному пункту',
-    'TooMuchArguments': 'Передано слишком много аргументов',
-    'NegativeStepException': 'Шаг должен быть положительным',
-    'NegativeAccuracy': 'Отрицательная погрешность вычислений'
-}
